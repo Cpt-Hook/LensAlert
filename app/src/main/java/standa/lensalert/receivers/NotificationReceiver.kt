09d0fa54f4ccb71f4cd8ef16dcf6ca0a1412c1b3
standa <ondra.stanicek@gmail.com>
2018-10-07 22:25:26 +0200

    update preferences before notification

--- app/src/main/java/standa/lensalert/receivers/NotificationReceiver.kt
+++ app/src/main/java/standa/lensalert/receivers/NotificationReceiver.kt
@@ -5,29 +5,34 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import standa.lensalert.NotificationFactory
-import standa.lensalert.PreferencesManager
-import standa.lensalert.isAfterYesterday
-import standa.lensalert.lensesWornOut
+import standa.lensalert.*
 
 class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.i("NotificationReceiver", "run")
+        if (intent.action == ACTION_SEND_NOTIFICATION) {
+            val preferences = PreferencesManager(context)
+            var updated = false
 
-        val preferences = PreferencesManager(context)
-        if(intent.action == ACTION_SEND_NOTIFICATION){
+            val doUpdate: (Int) -> Unit = {
+                if (!updated) {
+                    updated = true
+                    val notificationHandler = NotificationFactory(context)
+                    val notification = if (!preferences.lensesWornOut() && !preferences.date.isAfterYesterday()) notificationHandler.promptNotification
+                    else if (preferences.lensesWornOut()) notificationHandler.wornLensesNotificationHandler
+                    else null
+                    if (notification != null)
+                        (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                                .notify(NotificationFactory.NOTIFICATION_ID, notification)
+                }
+            }
 
-            val notificationHandler = NotificationFactory(context)
-            val notification =  if(!preferences.lensesWornOut() && !preferences.date.isAfterYesterday()) notificationHandler.promptNotification
-                                else if(preferences.lensesWornOut()) notificationHandler.wornLensesNotificationHandler
-                                else null
-
-            if(notification != null)
-                (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
-                        .notify(NotificationFactory.NOTIFICATION_ID, notification)
+            val task = SyncPreferencesTask(SyncPreferencesTask.getBasicHandler(context, preferences, postExecute = doUpdate))
+            task.execute()
         }
     }
+
 
     companion object {
         const val ACTION_SEND_NOTIFICATION = "standa.lensalert.receivers.ACTION_SEND_NOTIFICATION"
